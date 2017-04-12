9bbfd404e4f82e6ac28fceb2e8a64fbc4e229098
n8fr8 <nathan@freitas.net>
2017-04-12 12:46:26 -0400

    setup a passthrough application for CleanInsights, since we are already a SugarApp

--- app/src/main/java/net/opendasharchive/openarchive/OpenArchiveApp.java
+++ app/src/main/java/net/opendasharchive/openarchive/OpenArchiveApp.java
@@ -2,6 +2,7 @@ package net.opendasharchive.openarchive;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 import info.guardianproject.netcipher.proxy.OrbotHelper;
 import info.guardianproject.netcipher.proxy.StatusCallback;
@@ -33,6 +34,39 @@ public class OpenArchiveApp extends com.orm.SugarApp {
     private void initInsights ()
     {
         CleanInsights cim = CleanInsights.getInstance(this);
+
+        //setup a passthrough application for CleanInsights, since we are already a SugarApp
+        CleanInsightsApplication cleanInsightsApplication = new CleanInsightsApplication() {
+
+            @Override
+            public Context getApplicationContext() {
+                return OpenArchiveApp.this.getApplicationContext();
+            }
+
+            @Override
+            public SharedPreferences getSharedPreferences(String name, int mode) {
+                return OpenArchiveApp.this.getSharedPreferences(name, mode);
+            }
+
+            @Override
+            public String getMeasureUrl() {
+                return "https://demo.cleaninsights.io";
+            }
+
+            @Override
+            public String getMeasureUrlCertificatePin()
+            {
+                //generate your own using this tool: https://github.com/scottyab/ssl-pin-generator
+                return "sha256/ZG+5y3w2mxstotmn15d9tnJtwss591+L6EH/yJMF41I=";
+            }
+
+            @Override
+            public Integer getSiteId() {
+                return 1;
+            }
+        };
+
+        cim.initPwiki(cleanInsightsApplication);
     }
 
     public boolean checkTor ()
