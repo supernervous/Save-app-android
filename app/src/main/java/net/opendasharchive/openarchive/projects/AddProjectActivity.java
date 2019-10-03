f5da546cd1017319aa45f825449caa4dd8b4ba96
n8fr8 <nathan@guardianproject.info>
2019-10-03 17:05:00 -0400

    if an account/space is not setup, don't add a project fixes #130

--- app/src/main/java/net/opendasharchive/openarchive/projects/AddProjectActivity.java
+++ app/src/main/java/net/opendasharchive/openarchive/projects/AddProjectActivity.java
@@ -2,6 +2,7 @@ package net.opendasharchive.openarchive.projects;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -10,6 +11,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import net.opendasharchive.openarchive.R;
+import net.opendasharchive.openarchive.onboarding.FirstStartActivity;
+import net.opendasharchive.openarchive.services.WebDAVSiteController;
+
+import io.scal.secureshareui.model.Account;
 
 public class AddProjectActivity extends AppCompatActivity {
 
@@ -27,13 +32,31 @@ public class AddProjectActivity extends AppCompatActivity {
 
     public void onNewProjectClicked (View view) {
 
-        Intent intent = new Intent(this,CreateNewProjectActivity.class);
-        startActivityForResult(intent,1000);
+        Account account = new Account(this, WebDAVSiteController.SITE_NAME);
+        if (account != null && (!TextUtils.isEmpty(account.getSite()))) {
+            Intent intent = new Intent(this, CreateNewProjectActivity.class);
+            startActivityForResult(intent, 1000);
+        }
+        else
+        {
+            finish();
+            startActivity(new Intent(this, FirstStartActivity.class));
+        }
     }
 
     public void onBrowseProjects (View view) {
-        Intent intent = new Intent(this,BrowseProjectsActivity.class);
-        startActivityForResult(intent,1001);
+
+        Account account = new Account(this, WebDAVSiteController.SITE_NAME);
+        if (account != null && (!TextUtils.isEmpty(account.getSite()))) {
+            Intent intent = new Intent(this, BrowseProjectsActivity.class);
+            startActivityForResult(intent, 1001);
+        }
+        else
+        {
+            finish();
+            startActivity(new Intent(this, FirstStartActivity.class));
+        }
+
     }
 
     @Override
