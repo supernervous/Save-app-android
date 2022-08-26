85de7bb5802c4b904d34dca45153865c6f9de160
pratik <prats.khive@gmail.com>
2022-08-26 12:39:37 -0400

    add deleted file..oops

---
+++ app/src/main/java/net/opendasharchive/openarchive/publish/PublishJobService.java
@@ -0,0 +1,27 @@
+package net.opendasharchive.openarchive.publish;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+import net.opendasharchive.openarchive.db.Media;
+
+import java.util.List;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class PublishJobService extends JobService {
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+
+        List<Media> results = Media.find(Media.class, "status = ?", Media.STATUS_QUEUED + "");
+        //if (results.size() > 0)
+        //ContextCompat.startForegroundService(this, new Intent(this, PublishService.class));
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
