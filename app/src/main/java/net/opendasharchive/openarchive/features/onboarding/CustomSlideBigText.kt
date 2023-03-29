f4f145d23cfc56c87ceaf45b6424bf754e708fb6
Benjamin Erhart <berhart@netzarchitekten.com>
2023-03-29 15:32:56 +0200

    Cleaned up intro, updated AppIntro dependency, update Tor scene state dynamically. Only force Tor, if user explicitly starts it.

--- app/src/main/java/net/opendasharchive/openarchive/features/onboarding/CustomSlideBigText.kt
+++ app/src/main/java/net/opendasharchive/openarchive/features/onboarding/CustomSlideBigText.kt
@@ -1,71 +1,47 @@
 package net.opendasharchive.openarchive.features.onboarding
 
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
-import net.opendasharchive.openarchive.R
+import net.opendasharchive.openarchive.databinding.CustomSlideBigTextBinding
 
 class CustomSlideBigText : Fragment() {
 
-    private var layoutResId = 0
-    private var mTitle: String? = null
-    private var mButtonText: String? = null
-    private var mSubTitle: String? = null
-    private var mButtonListener: View.OnClickListener? = null
+    private var mTitle = 0
+    private var mSubTitle = 0
 
-    fun setTitle(title: String) {
-        mTitle = title
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mTitle = arguments?.getInt(ARG_LAYOUT_TITLE) ?: 0
+        mSubTitle = arguments?.getInt(ARG_LAYOUT_SUBTITLE) ?: 0
     }
 
-    fun setSubTitle(subTitle: String) {
-        mSubTitle = subTitle
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = CustomSlideBigTextBinding.inflate(inflater, container, false)
 
-    fun showButton(buttonText: String, buttonListener: View.OnClickListener) {
-        mButtonText = buttonText
-        mButtonListener = buttonListener
-    }
+        binding.customSlideBigText.setText(mTitle)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        arguments?.let {
-            if (it.containsKey(ARG_LAYOUT_RES_ID)) {
-                layoutResId = it.getInt(ARG_LAYOUT_RES_ID)
-            }
+        if (mSubTitle != 0) {
+            binding.customSlideBigTextSub.setText(mSubTitle)
+            binding.customSlideBigTextSub.visibility = View.VISIBLE
         }
 
-        val view = inflater.inflate(layoutResId, container, false)
-        (view.findViewById(R.id.custom_slide_big_text) as TextView).text = mTitle
-
-        if (!TextUtils.isEmpty(mSubTitle)) {
-            val tv = view.findViewById(R.id.custom_slide_big_text_sub) as TextView
-            tv.text = mSubTitle
-            tv.visibility = View.VISIBLE
-        }
-
-        if (mButtonText != null) {
-            val button = view.findViewById(R.id.custom_slide_button) as Button
-            button.visibility = View.VISIBLE
-            button.text = mButtonText
-            button.setOnClickListener(mButtonListener)
-        }
-        return view
+        return binding.root
     }
 
     companion object {
-        private const val ARG_LAYOUT_RES_ID = "layoutResId"
 
-        fun newInstance(@LayoutRes layoutResId: Int): CustomSlideBigText = CustomSlideBigText().also {
-            val bundle = Bundle()
-            bundle.putInt(ARG_LAYOUT_RES_ID, layoutResId)
-            it.arguments = bundle
+        private const val ARG_LAYOUT_TITLE = "layoutTitle"
+        private const val ARG_LAYOUT_SUBTITLE = "layoutSubtitle"
+
+        fun newInstance(title: Int, subtitle: Int): CustomSlideBigText = CustomSlideBigText().also {
+            it.arguments = Bundle().apply {
+                putInt(ARG_LAYOUT_TITLE, title)
+                putInt(ARG_LAYOUT_SUBTITLE, subtitle)
+            }
         }
-
     }
-
 }
--- app/src/main/java/net/opendasharchive/openarchive/features/onboarding/OAAppIntro.kt
+++ app/src/main/java/net/opendasharchive/openarchive/features/onboarding/OAAppIntro.kt
@@ -1,19 +1,13 @@
 package net.opendasharchive.openarchive.features.onboarding
 
 import android.content.Intent
-import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.View
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.github.paolorotolo.appintro.AppIntro
-import info.guardianproject.netcipher.proxy.OrbotHelper
+import com.github.appintro.AppIntro
 import net.opendasharchive.openarchive.R
-import net.opendasharchive.openarchive.features.onboarding.CustomOnboardingScreen.Companion.newInstance
-import net.opendasharchive.openarchive.features.onboarding.CustomSlideBigText.Companion.newInstance
-import net.opendasharchive.openarchive.util.Prefs
 
 /**
  * Created by n8fr8 on 8/3/16.
@@ -22,123 +16,56 @@ class OAAppIntro : AppIntro() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-        setFadeAnimation()
 
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
-        //addSlide(AppIntroFragment.newInstance(getString(R.string.title_welcome), getString(R.string.onboarding_intro), R.drawable.oafeature, getResources().getColor(R.color.oablue)));
+        addSlide(CustomSlideBigText.newInstance(R.string.onboarding_intro, R.string.app_tag_line))
 
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
+        addSlide(CustomOnboardingScreen.newInstance(R.string.oa_title_1,
+                R.string.oa_subtitle_1, R.drawable.onboarding1))
 
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
-        //addSlide(AppIntroFragment.newInstance(getString(R.string.title_welcome), getString(R.string.onboarding_intro), R.drawable.oafeature, getResources().getColor(R.color.oablue)));
+        addSlide(CustomOnboardingScreen.newInstance(R.string.oa_title_2,
+                R.string.oa_subtitle_2, R.drawable.onboarding2))
 
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
-        val welcome = newInstance(R.layout.custom_slide_big_text)
-        welcome.setTitle(getString(R.string.onboarding_intro))
-        welcome.setSubTitle(getString(R.string.app_tag_line))
+        addSlide(CustomOnboardingScreen.newInstance(R.string.oa_title_3,
+                R.string.oa_subtitle_3, R.drawable.onboarding3))
 
-        addSlide(welcome)
+        addSlide(CustomOnboardingScreen.newInstance(R.string.oa_title_4,
+                R.string.oa_subtitle_4, R.drawable.onboarding4))
 
-        addSlide(
-            newInstance(
-                R.layout.custom_onboarding_main,
-                getString(R.string.oa_title_1),
-                getString(R.string.oa_subtitle_1),
-                R.drawable.onboarding1
-            )
-        )
-        addSlide(
-            newInstance(
-                R.layout.custom_onboarding_main,
-                getString(R.string.oa_title_2),
-                getString(R.string.oa_subtitle_2),
-                R.drawable.onboarding2
-            )
-        )
-        addSlide(
-            newInstance(
-                R.layout.custom_onboarding_main,
-                getString(R.string.oa_title_3),
-                getString(R.string.oa_subtitle_3),
-                R.drawable.onboarding3
-            )
-        )
-        addSlide(
-            newInstance(
-                R.layout.custom_onboarding_main,
-                getString(R.string.oa_title_4),
-                getString(R.string.oa_subtitle_4),
-                R.drawable.onboarding4
-            )
-        )
-
-        if (!OrbotHelper.isOrbotInstalled(this)) {
-            val cos = newInstance(
-                R.layout.custom_onboarding_main,
-                getString(R.string.onboarding_archive_over_tor),
-                getString(R.string.onboarding_archive_over_tor_install_orbot),
-                R.drawable.onboarding5
-            )
-            addSlide(cos)
-            cos.enableButton(getString(R.string.action_install), View.OnClickListener {
-                Prefs.setUseTor(true)
-                startActivity(OrbotHelper.getOrbotInstallIntent(this@OAAppIntro))
-            })
-        } else {
-            val cos = newInstance(
-                R.layout.custom_onboarding_main,
-                getString(R.string.onboarding_archive_over_tor),
-                getString(R.string.archive_over_tor_enable_orbot),
-                R.drawable.onboarding5
-            )
-            addSlide(cos)
-            cos.enableButton(getString(R.string.action_start), View.OnClickListener {
-                Prefs.setUseTor(true)
-                OrbotHelper.requestStartTor(this@OAAppIntro)
-                finish()
-                startActivity(Intent(this@OAAppIntro, SpaceSetupActivity::class.java))
-            })
-        }
+        addSlide(TorOnboardingScreen())
 
         setColorDoneText(ContextCompat.getColor(this, R.color.white))
         setSeparatorColor(ContextCompat.getColor(this, R.color.white))
         setBarColor(ContextCompat.getColor(this, R.color.oablue))
-        // Hide Skip/Done button.
-        showSkipButton(false)
+        showStatusBar(true)
+        setStatusBarColor(ContextCompat.getColor(this, R.color.oablue))
+        setNavBarColor(ContextCompat.getColor(this, R.color.oablue))
 
-        isProgressButtonEnabled = true
+        // Hide Skip/Done button.
+        isSkipButtonEnabled = false
+
+        isSystemBackButtonLocked = true
     }
 
     override fun onSkipPressed(currentFragment: Fragment?) {
         super.onSkipPressed(currentFragment)
-        // Do something when users tap on Skip button.
+
         finish()
     }
 
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            val obscuredTouch = event!!.flags and MotionEvent.FLAG_WINDOW_IS_PARTIALLY_OBSCURED != 0
-            if (obscuredTouch) return false
+            if ((event?.flags ?: 0) and MotionEvent.FLAG_WINDOW_IS_PARTIALLY_OBSCURED != 0) return false
         }
+
         return super.dispatchTouchEvent(event)
     }
 
     override fun onDonePressed(currentFragment: Fragment?) {
         super.onDonePressed(currentFragment)
-        // Do something when users tap on Done button.
+
         finish()
         startActivity(Intent(this, SpaceSetupActivity::class.java))
     }
-
-    override fun onSlideChanged(oldFragment: Fragment?, newFragment: Fragment?) {
-        super.onSlideChanged(oldFragment, newFragment)
-        // Do something when the slide changes.
-    }
-
 }
