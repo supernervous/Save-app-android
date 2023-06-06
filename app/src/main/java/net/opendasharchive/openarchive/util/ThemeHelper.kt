5d5b39cabe5c29e94ec2b3ed34d6ab37f0818b83
Michael Pöhn <michael@poehn.at>
2023-06-06 18:44:22 +0200

    🧹 ThemeHelper: object instead of companion object

--- app/src/main/java/net/opendasharchive/openarchive/util/ThemeHelper.kt
+++ app/src/main/java/net/opendasharchive/openarchive/util/ThemeHelper.kt
@@ -4,15 +4,13 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import net.opendasharchive.openarchive.R
 
-class ThemeHelper {
-    companion object {
-        fun setTheme(context: Context, theme: String)  {
-            val mode = when(theme) {
-                context.getString(R.string.prefs_theme_val_light) -> AppCompatDelegate.MODE_NIGHT_NO;
-                context.getString(R.string.prefs_theme_val_dark) -> AppCompatDelegate.MODE_NIGHT_YES;
-                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
-            }
-            AppCompatDelegate.setDefaultNightMode(mode);
-        };
-    }
+object ThemeHelper {
+    fun setTheme(context: Context, theme: String) {
+        val mode = when (theme) {
+            context.getString(R.string.prefs_theme_val_light) -> AppCompatDelegate.MODE_NIGHT_NO;
+            context.getString(R.string.prefs_theme_val_dark) -> AppCompatDelegate.MODE_NIGHT_YES;
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+        }
+        AppCompatDelegate.setDefaultNightMode(mode);
+    };
 }
