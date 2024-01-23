04164d8109ec9db539456c9cf8f6ec69116ae505
Michael Pöhn <michael@poehn.at>
2024-01-23 03:29:32 +0100

    🧅 add tor support for gdrive
    This change adds support for routing traffic to Google Drive through Tor
    using Orbots HTTP proxy.

--- app/src/main/java/net/opendasharchive/openarchive/services/gdrive/GDriveConduit.kt
+++ app/src/main/java/net/opendasharchive/openarchive/services/gdrive/GDriveConduit.kt
@@ -8,11 +8,14 @@ import com.google.android.gms.common.api.Scope
 import com.google.api.client.extensions.android.http.AndroidHttp
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.client.googleapis.media.MediaHttpUploader
+import com.google.api.client.http.HttpTransport
 import com.google.api.client.http.InputStreamContent
+import com.google.api.client.http.apache.ApacheHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
 import com.google.api.services.drive.model.File
+import info.guardianproject.netcipher.proxy.OrbotHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -20,10 +23,45 @@ import net.opendasharchive.openarchive.R
 import net.opendasharchive.openarchive.db.Media
 import net.opendasharchive.openarchive.features.folders.BrowseFoldersViewModel
 import net.opendasharchive.openarchive.services.Conduit
+import net.opendasharchive.openarchive.util.Prefs
+import org.apache.http.conn.ClientConnectionManager
+import org.apache.http.conn.params.ConnManagerParams
+import org.apache.http.conn.params.ConnPerRouteBean
+import org.apache.http.conn.scheme.PlainSocketFactory
+import org.apache.http.conn.scheme.Scheme
+import org.apache.http.conn.scheme.SchemeRegistry
+import org.apache.http.conn.ssl.SSLSocketFactory
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler
+import org.apache.http.impl.conn.ProxySelectorRoutePlanner
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager
+import org.apache.http.params.BasicHttpParams
+import org.apache.http.params.HttpConnectionParams
 import timber.log.Timber
+import java.io.IOException
 import java.io.InputStream
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.net.ProxySelector
+import java.net.SocketAddress
+import java.net.URI
 import java.util.Date
 
+/**
+ * This class contains all communication with / integration of Google Drive
+ *
+ * The only actually working documentation I could find about Googles Android GDrive API is this:
+ * https://stackoverflow.com/questions/56949872/
+ * There's also this official documentation by Google for accessing GDrive, however it was pretty
+ * useless to me, since it doesn't explain what's going on at all. (I also couldn't get it to run
+ * in a reasonable amount of time):
+ * https://github.com/googleworkspace/android-samples/tree/master/drive/deprecation
+ * The official documentation doesn't mention Android and the Java Sample is only useful for
+ * integrating GDrive into backends. However it's still helpful for figuring building queries:
+ * https://developers.google.com/drive/api/guides/about-sdk
+ * Another important resource is this official guide on authenticating an Android app with Google:
+ * https://developers.google.com/identity/sign-in/android/start-integrating
+ */
 class GDriveConduit(media: Media, context: Context) : Conduit(media, context) {
 
     private var mDrive: Drive = getDrive(mContext)
@@ -52,9 +90,53 @@ class GDriveConduit(media: Media, context: Context) : Conduit(media, context) {
             // Timber.v("GDriveConduit.getDrive(): credential.selectedAccount ${credential.selectedAccount}")
             // Timber.v("GDriveConduit.getDrive(): credential.selectedAccount.name ${credential.selectedAccount?.name}")
 
-            // TODO: add optional transport proxy config (tor/onion routing support)
+            val transport: HttpTransport = if (Prefs.useTor) {
+                // initialization code copied from: ApacheHttpTransport.newDefaultHttpParams()
+                // This is the simplest solution I could come up with for actually sending traffic
+                // to GDrive through Tor. Note that all calls to deprecated functions are copied
+                // from the only known to work version of GDrive API.
+                val params = BasicHttpParams()
+                HttpConnectionParams.setStaleCheckingEnabled(params, false)
+                HttpConnectionParams.setSocketBufferSize(params, 8192)
+                ConnManagerParams.setMaxTotalConnections(params, 200)
+                ConnManagerParams.setMaxConnectionsPerRoute(params, ConnPerRouteBean(20))
+                val registry = SchemeRegistry()
+                registry.register(Scheme("http", PlainSocketFactory.getSocketFactory(), 80))
+                registry.register(Scheme("https", SSLSocketFactory.getSocketFactory(), 443))
+                val connectionManager: ClientConnectionManager =
+                    ThreadSafeClientConnManager(params, registry)
+                val defaultHttpClient = DefaultHttpClient(connectionManager, params)
+                defaultHttpClient.httpRequestRetryHandler = DefaultHttpRequestRetryHandler(0, false)
+                val proxySelector = object : ProxySelector() {
+                    override fun select(uri: URI?): MutableList<Proxy> {
+                        return mutableListOf(
+                            // tried SOCKS here, but in my tests when specifying SOCKS, the uploads
+                            // seamed to bypass proxy settings altogether and connect directly instead
+                            Proxy(
+                                Proxy.Type.HTTP,
+                                InetSocketAddress(
+                                    OrbotHelper.DEFAULT_PROXY_HOST,
+                                    OrbotHelper.DEFAULT_PROXY_HTTP_PORT
+                                )
+                            )
+                        )
+                    }
 
-            return Drive.Builder(AndroidHttp.newCompatibleTransport(), GsonFactory(), credential)
+                    override fun connectFailed(uri: URI?, sa: SocketAddress?, ioe: IOException?) {
+                        Timber.e("proxy connection Failed ($uri, $sa)", ioe)
+                    }
+                }
+                defaultHttpClient.routePlanner = ProxySelectorRoutePlanner(
+                    registry,
+                    proxySelector
+                )
+
+                ApacheHttpTransport(defaultHttpClient)
+            } else {
+                AndroidHttp.newCompatibleTransport()
+            }
+
+            return Drive.Builder(transport, GsonFactory(), credential)
                 .setApplicationName(ContextCompat.getString(context, R.string.app_name)).build()
         }
 
@@ -92,7 +174,7 @@ class GDriveConduit(media: Media, context: Context) : Conduit(media, context) {
             }
         }
 
-        fun listFoldersInRoot(gdrive: Drive) : List<BrowseFoldersViewModel.Folder>{
+        fun listFoldersInRoot(gdrive: Drive): List<BrowseFoldersViewModel.Folder> {
             val result = ArrayList<BrowseFoldersViewModel.Folder>()
             try {
                 var pageToken: String? = null
