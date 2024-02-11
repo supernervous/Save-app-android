56ef76abf81e307c60d3ded045c33f0859dbda37
Ryan Jennings <rybertjen@gmail.com>
2024-02-10 20:48:00 -0800

    fix(theme): changing theme breaks screens #531
    MainActivity
    - upgraded the viewpager to google's viewpager2
    
    MainMediaFragment;
    - moved broadcast listener from activity
    - moved delete options menu item from activity
    
    ProjectAdapter:
    - changed to viewpager2 adapter base class
    - moved some item logic from activity

--- app/src/main/java/net/opendasharchive/openarchive/features/main/ProjectAdapter.kt
+++ app/src/main/java/net/opendasharchive/openarchive/features/main/ProjectAdapter.kt
@@ -1,68 +1,61 @@
 package net.opendasharchive.openarchive.features.main
 
-import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import net.opendasharchive.openarchive.db.Project
 import net.opendasharchive.openarchive.features.settings.SettingsFragment
-import net.opendasharchive.openarchive.util.SmartFragmentStatePagerAdapter
-import timber.log.Timber
 import kotlin.math.max
 
-class ProjectAdapter(fragmentManager: FragmentManager) : SmartFragmentStatePagerAdapter(fragmentManager) {
+class ProjectAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
 
     var projects = listOf<Project>()
         private set
-
-    override fun getItem(position: Int): Fragment {
-        if (position == settingsIndex) {
-            return SettingsFragment()
-        }
-
-        val project = getProject(position)
-
-        return MainMediaFragment.newInstance(project?.id ?: -1)
-    }
-
-    override fun getCount(): Int {
-        return max(1, projects.size) + 1
-    }
 
     fun getProject(i: Int): Project? {
         return if (i > -1 && i < projects.size) projects[i] else null
     }
 
     val settingsIndex: Int
-        get() = count - 1
+        get() = projects.size
 
     fun updateData(projects: List<Project>) {
         this.projects = projects
-
-        notifyDataSetChanged()
+        notifyItemRangeChanged(0, projects.size)
     }
 
-    fun getIndex(project: Project?): Int {
-        if (project == null) {
-            return 0
-        }
-
-        return (projects.indexOf(project)).coerceAtLeast(0)
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
+    fun getPageTitle(position: Int): CharSequence? {
         return getProject(position)?.description
     }
 
-    fun getRegisteredMediaFragment(position: Int): MainMediaFragment? {
-        return getRegisteredFragment(position) as? MainMediaFragment
+    override fun getItemCount(): Int {
+        return max(1, projects.size) + 1
     }
 
-    override fun restoreState(state: Parcelable?, loader: ClassLoader?) {
-        try {
-            super.restoreState(state, loader)
+    fun getProjectIndexById(id: Long, default: Int = 0): Int {
+        val index = projects.indexOfFirst { it.id == id }
+        return when (index) {
+            -1 -> default
+            else -> index
         }
-        catch (e: Exception) {
-            Timber.e("restoreState failed", e)
+    }
+
+    fun notifyProjectChanged(project: Project) {
+        val index = projects.indexOf(project)
+        if (index != -1) {
+            notifyItemChanged(index)
+        }
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            settingsIndex -> SettingsFragment()
+            else -> {
+                val project = getProject(position)
+                return MainMediaFragment.newInstance(project?.id ?: -1)
+            }
         }
     }
 }
